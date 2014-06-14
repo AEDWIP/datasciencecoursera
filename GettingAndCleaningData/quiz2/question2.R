1ddf9f6e86947c61485c83e2cc6363dd70fab060
Andy Davidson <andy_davidson@apple.com>
2014-06-14 10:57:19 -0700

    completed

---
+++ GettingAndCleaningData/quiz2/question2.R
@@ -0,0 +1,21 @@
+
+# The sqldf package allows for execution of SQL commands on R data frames. We will use the sqldf package to practice 
+# the queries we might send with the dbSendQuery command in RMySQL. Download the American Community Survey data and
+# load it into an R object called acs
+
+#
+# install.packages("sqldf")
+library(sqldf)
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+dataDir <- "data/"
+dataFile <- paste(dataDir, "americanCommunitServey.csv", "") # paste is really string concatenation
+if (!file.exists(dataDir)) { dir.create(dataDir)}
+if (!file.exists(dataFile)) {download.file(fileURL, destfile=dataFile, method="curl")}
+
+# data.frames are slower than data.tables. say use data.frame. Lets see if table work
+library(data.table)
+acsDF <- read.csv(dataFile)
+acs <- data.table(acsDF)
+
+# Which of the following commands will select only the data for the probability weights pwgtp1 with ages less than 50?
+sqldf("select pwgtp1 from acs where AGEP < 50")
