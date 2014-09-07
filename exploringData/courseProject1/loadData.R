2ca5968efe141ea3acbf433376cd149c79951a61
Andy Davidson <andy_davidson@apple.com>
2014-09-06 18:19:18 -0700

    completed plot 1

--- exploringData/courseProject1/loadData.R
+++ exploringData/courseProject1/loadData.R
@@ -1,4 +1,9 @@
+#
+# Andy@SantaCruzIntegration.com
+# course project 1
+#
 
+#
 # first just read the first 10 rows to see if read.table args are correct
 # allData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", nrows = 100 )
 allData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?")
@@ -17,6 +22,16 @@ allData$timeStamp <- strptime(timeStamp, format="%d/%m/%Y %H:%M:%S")
 #end <- strptime("16/12/2006 18:16:00", format="%d/%m/%Y %H:%M:%S") # index 53
 #z <- y[ (y <= begin) & (y <= end) ] 
 
-begin <- strptime("01/02/2006 00:00:00", format="%d/%m/%Y %H:%M:%S")
-end <- strptime("01/03/2006 00:00:00", format="%d/%m/%Y %H:%M:%S") 
+begin <- strptime("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S")
+end <- strptime("03/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S") 
 febData <- allData[(allData$timeStamp >= begin) & (allData$timeStamp < end) ,]
+
+# 
+# some one posted this solution much simpler
+# hpc <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", 
+#                 stringsAsFactors=FALSE)
+# hpc2 <- hpc[min(which(hpc$Date == "1/2/2007")):max(which(hpc$Date == "2/2/2007")),]
+#
+
+rm(allData) # clean up memory
+
