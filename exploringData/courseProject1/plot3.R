14208267070701568fc02e8ebcfb66fd897e0013
Andy Davidson <andy_davidson@apple.com>
2014-09-06 20:56:35 -0700

    plot.3 is complete

--- exploringData/courseProject1/plot3.R
+++ exploringData/courseProject1/plot3.R
@@ -12,6 +12,7 @@ source("loadData.R")
 #
 levels <- c("s1", "s2", "s3")
 name <- c("sub_metering", "timeStamp", "meterNum")
+
 sm1 <- data.frame(febData$Sub_metering_1, febData$timeStamp, factor(rep("s1", nrow(febData)), levels))
 names(sm1) <- name;
 
@@ -27,7 +28,7 @@ plotData <- rbind(sm1, sm2, sm3)
 # graph the data
 #
 
-#png("plot3.png", width = 480, height = 480, bg="grey")
+png("plot3.png", width = 480, height = 480, bg="grey")
 
 par(bg="grey")
 plot(plotData$timeStamp, plotData$sub_metering, type='n', xlab="", ylab="Energy sub metering")
@@ -35,12 +36,6 @@ lines(plotData$timeStamp[plotData$meterNum == "s1"], plotData$sub_meter[plotData
 lines(plotData$timeStamp[plotData$meterNum == "s2"], plotData$sub_meter[plotData$meterNum == "s2"], type = "l", col = "red")
 lines(plotData$timeStamp[plotData$meterNum == "s3"], plotData$sub_meter[plotData$meterNum == "s3"], type = "l", col = "blue")
 
-# with(plotData, plot(plotData$Sub_metering, plotData$timeStamp, xlab="", ylab="Energy sub metering", type="n")) # do not plot yet
-# 
-# with(subset(plotData, meterNum == "s1"), points(plotData$timeStamp, plotData$Sub_metering, col="black")) 
-# with(subset(plotData, meterNum == "s2"), points(plotData$timeStamp, plotData$Sub_metering, col="red")) 
-# with(subset(plotData, meterNum == "s3"), points(plotData$timeStamp, plotData$Sub_metering, col="blue")) 
-# 
 legend("topright", legend=c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3"), lwd=1, col=c("black", "red", "blue"))
 
-#dev.off() # flush and close file
+dev.off() # flush and close file
