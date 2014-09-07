ff795347b5e9020b0e6b8881e9db106e741cac19
Andy Davidson <andy_davidson@apple.com>
2014-09-06 19:52:44 -0700

    added legend

--- exploringData/courseProject1/plot3.R
+++ exploringData/courseProject1/plot3.R
@@ -1,18 +1,21 @@
 
 #
 # Andy@SantaCruzIntegration.com
-# course project 1, plot 2
+# course project 1, plot 3
 #
 
 source("loadData.R")
-png("plot2.png", width = 480, height = 480, bg="grey")
+#png("plot3.png", width = 480, height = 480, bg="grey")
 
 par(bg="grey")
+
 
 # add the data but do not plot it yet
 plot(febData$timeStamp, febData$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="n") 
 
+legend("topright", legend=c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3"), lwd=1, col=c("black", "red", "blue"))
+
 # take the data  and joining the corresponding points with line segments.
 lines(febData$timeStamp, febData$Global_active_power)
 
-dev.off() # flush and close file
+#dev.off() # flush and close file
