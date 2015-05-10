f2c80853508c48dbb6511b888e6b6dbcba5105f6
Andy Davidson <andy_davidson@apple.com>
2015-05-10 12:09:31 -0700

    added notes

---
+++ regressionModels/understandingRhoAndSlope.R
@@ -0,0 +1,14 @@
+#
+# if data is centered data rho == slope
+#
+
+x <- c(1:10)
+y <- x * 3
+cor(y, x)
+coef(lm(y ~ x))
+
+xc <- (x - mean(x)) / sd(x)
+yc <- (y - mean(y)) / sd(y)
+
+cor(yc, xc)
+coef(lm(yc ~ xc))
