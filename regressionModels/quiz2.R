862ac5d0a9d1ca82825a40139d496ed25fbdeb6d
Andy Davidson <andy_davidson@apple.com>
2015-05-17 12:55:55 -0700

    I think I have the answer to 3 how ever manual calculation does not agree with answer post on formum

--- regressionModels/quiz2.R
+++ regressionModels/quiz2.R
@@ -18,4 +18,35 @@ s[2,4]
 
 #
 # q2
+# Consider the previous problem, give the estimate of the residual standard 
+# deviation.
 #
+summary(fit)$sigma
+
+# answer 0.223
+
+#
+# q3
+# In the mtcars data set, fit a linear regression model of weight (predictor) 
+# on mpg (outcome). Get a 95% confidence interval for the expected mpg at the 
+# average weight. What is the lower endpoint?
+#
+y <- mtcars$mpg
+x <- mtcars$wt
+fit <- lm(y ~ x)
+resd <- summary(fit)$sigma # residual error standard devation
+newX <- mean(x)
+newY <- predict(fit, newdata = data.frame(x=newX), interval=("confidence"))
+newY
+#> newY
+#fit      lwr      upr
+#1 20.09062 18.99098 21.19027
+#> 
+alpha = 0.05
+newYValue = newY[1]
+# this did not work? ci <- newYValue + c(-1, 1) * qt(1 - alpha/2, df=fit$df) * resd
+
+# how I hacked the answer from given choices
+# newY - 18.991
+# newY + c(-1,1) *  1.099625
+# answer = 18.99100
