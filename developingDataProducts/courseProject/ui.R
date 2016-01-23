b60c78b9ed12d79601ffb8945465719f0d14c87c
Andy Davidson <andy_davidson@apple.com>
2016-01-23 15:43:19 -0800

    init commit

--- developingDataProducts/courseProject/ui.R
+++ developingDataProducts/courseProject/ui.R
@@ -1,6 +1,9 @@
 library( shiny) 
 shinyUI( pageWithSidebar( 
             headerPanel(" Hello Shiny!"), 
-            sidebarPanel( h3(' Sidebar text') ), 
+            sidebarPanel( 
+                h3(' Sidebar text'),
+                numericInput(' id1', 'Numeric input, labeled id1', 0, min = 62, max = 74, step = 0.5)
+            ), 
             mainPanel( h3(' Main Panel text') )
     ))
