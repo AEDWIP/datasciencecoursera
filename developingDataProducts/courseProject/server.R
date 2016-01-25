550b9fde7fc491804704af6e1083c5cad6cdcf3c
Andy Davidson <andy_davidson@apple.com>
2016-01-24 16:42:43 -0800

    shinny app displays galtons histgram and the variance graphs

--- developingDataProducts/courseProject/server.R
+++ developingDataProducts/courseProject/server.R
@@ -1,17 +1,37 @@
+# ref: http://web.stanford.edu/~cengel/cgi-bin/anthrospace/building-my-first-shiny-application-with-ggplot
 library(shiny) 
-library(UsingR) 
+library(UsingR) # I think this loads hist
+library(ggplot2) # load ggplot
+
 data(galton)
+library(datasets) # car and driver data set
+
+dat <- data.frame(
+    y = c(
+        dnorm(xvals, mean = 0, sd = 1),
+        dnorm(xvals, mean = 0, sd = 2),
+        dnorm(xvals, mean = 0, sd = 3),
+        dnorm(xvals, mean = 0, sd = 4)
+    ),
+    x = rep(xvals, 4),
+    factor = factor(rep(1 : 4, rep(length(xvals), 4)))
+)
 
 shinyServer( 
-        function(input, output) { 
-            output$newHist <- renderPlot({ 
-                                            hist(galton$child, xlab ='child height',col ='lightblue', main ='Histogram')
-                                            mu <- input$mu
-                                            lines(c( mu, mu), c( 0, 200), col ="red", lwd = 5)
-                                            mse <- mean((galton$child - mu) ^2)
-                                            text(63, 150, paste("mu = ", mu))
-                                            text( 63, 140, paste("MSE = ", round( mse, 2)))
-                                        })
-        }
+    function(input, output) { 
+        output$newHist <- renderPlot({ 
+                                    hist(galton$child, xlab ='child height',col ='lightblue', main ='Histogram')
+                                        mu <- input$mu
+                                        lines(c( mu, mu), c( 0, 200), col ="red", lwd = 5)
+                                        mse <- mean((galton$child - mu) ^2)
+                                        text(63, 150, paste("mu = ", mu))
+                                        text( 63, 140, paste("MSE = ", round( mse, 2)))
+                                    })
+        
+        output$varianceGraph <- renderPlot({ 
+            ggplot(dat, aes(x = x, y = y, color = factor)) + geom_line(size = 2)
+            })
+        
+    }
 )
 
--- developingDataProducts/courseProject/ui.R
+++ developingDataProducts/courseProject/ui.R
@@ -3,11 +3,16 @@ shinyUI( pageWithSidebar(
             headerPanel(" Hello Shiny!"), 
             
             sidebarPanel( 
-                h3(' Sidebar text'),
-                sliderInput('mu', 'Center of Mass', value = 65, min = 62, max = 74, step = 0.5)
+                h3('mu Sidebar text'),
+                sliderInput('mu', 'Center of Mass', value = 65, min = 62, max = 74, step = 0.5),                
+                
+                h3('variance Sidebar text'),
+                sliderInput('variance', 'variance', value = 1, min = -3, max = 3, step = 0.1)
             ), 
             
             mainPanel( 
-                plotOutput('newHist')
+                plotOutput('newHist'),
+                h3('AEDWIP'),
+                plotOutput('varianceGraph')
             )
     ))
