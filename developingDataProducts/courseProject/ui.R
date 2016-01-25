e07a56ad45c2c92786dc29a48d1a196cdbd45d38
Andy Davidson <andy_davidson@apple.com>
2016-01-24 16:56:23 -0800

    clean up

--- developingDataProducts/courseProject/ui.R
+++ developingDataProducts/courseProject/ui.R
@@ -3,15 +3,11 @@ shinyUI( pageWithSidebar(
             headerPanel(" Hello Shiny!"), 
             
             sidebarPanel( 
-                h3('mu Sidebar text'),
-                sliderInput('mu', 'Center of Mass', value = 65, min = 62, max = 74, step = 0.5),                
-                
                 h3('standard deviation'),
                 sliderInput('standarDeviation', 'Standard Deviation', value = 1, min = 0.1, max = 5, step = 0.1)
             ), 
             
             mainPanel( 
-                plotOutput('newHist'),
                 h3('AEDWIP'),
                 plotOutput('varianceGraph')
             )
